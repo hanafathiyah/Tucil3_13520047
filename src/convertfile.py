d41daefae16f71ab8f8cd166400f2b1f28f0172b
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-04-01 18:52:45 +0700

    convert file to matrix, has not been finished

---
+++ src/convertfile.py
@@ -0,0 +1,17 @@
+def convert_file_to_int_matrix(fifteen_puzzle_in_file):
+    int_matrix = [[0 for _ in range(4)] for _ in range(4)]
+    for i in range(4):
+        idx_col = 0
+        j = 0
+        while j < len(fifteen_puzzle_in_file[i]) - 1:
+            if(fifteen_puzzle_in_file[i][j] == " "):
+                j += 1;
+            elif(fifteen_puzzle_in_file[i][j] != " " and fifteen_puzzle_in_file[i][j+1] != " "): # 2 digits
+                int_matrix[i][idx_col] = int(fifteen_puzzle_in_file[i][j]) * 10 + int(fifteen_puzzle_in_file[i][j+1])
+                idx_col += 1;
+                j += 2;
+            elif(fifteen_puzzle_in_file[i][j] != " " and fifteen_puzzle_in_file[i][j+1] == " "): # 1 digit
+                int_matrix[i][idx_col] = int(fifteen_puzzle_in_file[i][j])
+                idx_col += 1;
+                j += 1;
+    return int_matrix
--- src/startgame.py
+++ src/startgame.py
@@ -1,7 +1,13 @@
 import imp
+from convertfile import convert_file_to_int_matrix
 from readfile import read_file
 
 def main():
     print("Welcome to 15-Puzzle Game")
     fifteen_puzzle_file_name = input("Enter the 15-Puzzle file name: ")
-    fifteen_puzzle = read_file(fifteen_puzzle_file_name)
+    fifteen_puzzle_in_file = read_file(fifteen_puzzle_file_name)
+    if(fifteen_puzzle_in_file == []):
+        exit()
+    print(fifteen_puzzle_in_file)
+    fifteen_puzzle = convert_file_to_int_matrix(fifteen_puzzle_in_file)
+    print(fifteen_puzzle)
