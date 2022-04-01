2962c9bee1fd8641ffd8fded131aa268a75aafc9
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-04-01 19:29:04 +0700

    add function get_empty_cell_idx and x_value

--- src/bnb.py
+++ src/bnb.py
@@ -1,2 +1,14 @@
+def get_empty_cell_idx(fifteen_puzzle):
+    for i in range(4):
+        for j in range(4):
+            if fifteen_puzzle[i][j] == 16:
+                return i,j
+
+def x_value(fifteen_puzzle):
+    if((get_empty_cell_idx(fifteen_puzzle)[0] + get_empty_cell_idx(fifteen_puzzle)[1]) % 2 == 0):
+        return 0
+    else:
+        return 1
+
 def procedure_bnb(fifteen_puzzle):
-    print(fifteen_puzzle)
+    print(x_value(fifteen_puzzle))
