f734acbac3ca5ebb5e66540d0e46428bf16164e5
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-04-01 19:09:59 +0700

    finishing convertfile.py

--- src/convertfile.py
+++ src/convertfile.py
@@ -4,14 +4,14 @@ def convert_file_to_int_matrix(fifteen_puzzle_in_file):
         idx_col = 0
         j = 0
         while j < len(fifteen_puzzle_in_file[i]) - 1:
-            if(fifteen_puzzle_in_file[i][j] == " "):
-                j += 1;
-            elif(fifteen_puzzle_in_file[i][j] != " " and fifteen_puzzle_in_file[i][j+1] != " "): # 2 digits
+            if(fifteen_puzzle_in_file[i][j] != " " and fifteen_puzzle_in_file[i][j+1] != " "): # 2 digits
                 int_matrix[i][idx_col] = int(fifteen_puzzle_in_file[i][j]) * 10 + int(fifteen_puzzle_in_file[i][j+1])
                 idx_col += 1;
-                j += 2;
+                j += 3;
             elif(fifteen_puzzle_in_file[i][j] != " " and fifteen_puzzle_in_file[i][j+1] == " "): # 1 digit
                 int_matrix[i][idx_col] = int(fifteen_puzzle_in_file[i][j])
                 idx_col += 1;
-                j += 1;
+                j += 2;
+        if(int_matrix[i][3] == 0):
+            int_matrix[i][3] = int(fifteen_puzzle_in_file[i][len(fifteen_puzzle_in_file[i])-1])
     return int_matrix
